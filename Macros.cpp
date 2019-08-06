c6c61099dff73efce455100b3c62fcd6c29c91b6
Srajan Tiwari <47104935+srajantiwari@users.noreply.github.com>
2019-08-07 00:20:26 +0530

    Boiler Plate Code

---
+++ Macros.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+
+using namespace __gnu_pbds;
+using namespace std;
+typedef long long ll;
+
+#define mem(dp,a)           memset(dp,a,sizeof dp)
+#define rep(i,a,b)          for(ll i=a;i<b;i++)
+#define pb(x)               push_back(x)
+#define mp(x,y)             make_pair(x,y)
+#define fastio              ios_base::sync_with_stdio(false);cin.tie(NULL)
+#define F                   first
+#define S                   second
+#define all(v)              (v).begin(),(v).end()
+#define pi                  3.14159265359
+ll INF=1e18+10;
+ll MOD=1000000007;
+
+int main()
+{
+    return 0;
+}
